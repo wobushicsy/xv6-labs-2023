cd5dab149f59a9fd7f7535f7185ff6c7d7bc0137
wobushicsy <csy657703828@163.com>
2024-02-07 20:13:30 +0800

    lab6 using threads done

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -14,6 +14,7 @@ struct entry {
   struct entry *next;
 };
 struct entry *table[NBUCKET];
+pthread_mutex_t locks[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
@@ -43,7 +44,10 @@ void put(int key, int value)
 
   // is the key already present?
   struct entry *e = 0;
-  for (e = table[i]; e != 0; e = e->next) {
+  pthread_mutex_t *lock = locks + i;
+  pthread_mutex_lock(lock);
+  e = table[i];
+  for (; e != 0; e = e->next) {
     if (e->key == key)
       break;
   }
@@ -54,7 +58,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(lock);
 }
 
 static struct entry*
@@ -64,7 +68,11 @@ get(int key)
 
 
   struct entry *e = 0;
-  for (e = table[i]; e != 0; e = e->next) {
+  pthread_mutex_t *lock = locks + i;
+  pthread_mutex_lock(lock);
+  e = table[i];
+  pthread_mutex_unlock(lock);
+  for (; e != 0; e = e->next) {
     if (e->key == key) break;
   }
 
@@ -110,6 +118,11 @@ main(int argc, char *argv[])
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
     exit(-1);
   }
+
+  for (int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(locks + i, NULL);
+  }
+
   nthread = atoi(argv[1]);
   tha = malloc(sizeof(pthread_t) * nthread);
   srandom(0);
